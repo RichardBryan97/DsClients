84542583abc016bac51d14f311aca43b1d127028
Richard Bryan <r_bryan@live.com>
2022-05-12 12:45:01 -0300

    Project Created

---
+++ backend/src/main/java/com/DsClients/ClientesBase/ClientesBaseApplication.java
@@ -0,0 +1,13 @@
+package com.DsClients.ClientesBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesBaseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/DsClients/ClientesBase/ClientesBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DsClients.ClientesBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
